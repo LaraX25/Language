684680b980e3e07718442d0b77b8f851eedffdf0
Larisa_Khisamova <Larisa_Khisamova@epam.com>
2024-02-03 12:52:48 +0100

    fix for checking by criteria

--- test_items.py
+++ test_items.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_login(browser):
-    browser.implicitly_wait(10)
+def test_check_button(browser):
     browser.get(link)
+    time.sleep(30)
     assert browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
     time.sleep(2)
