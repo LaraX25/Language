586ff201a0e647c16463126480252c8893e51157
Larisa_Khisamova <Larisa_Khisamova@epam.com>
2024-02-03 12:39:29 +0100

    initial commit

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_login(browser):
+    browser.implicitly_wait(10)
+    browser.get(link)
+    assert browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+    time.sleep(2)
